24213c59154c25a1fd1adcd273af5730e80ea950
sebastian <sebastian.rebmann2@dak.de>
2025-09-19 17:23:57 +0200

    fix

--- main.py
+++ main.py
@@ -9,48 +9,52 @@ pattern_rechnungsdatum = re.compile(r"Rechnungsdatum\s*(\d{2}\.\d{2}\.\d{4})")
 
 
 def extract_rechnungen(file):
-    umsatzsteuer = 0
-    with pdfplumber.open(file) as pdf:
-        page = pdf.pages[0]
-        text = page.extract_text()
-        print(text)
-        table = page.extract_table()
+    try:
+        umsatzsteuer = 0
+        with pdfplumber.open(file) as pdf:
+            page = pdf.pages[0]
+            text = page.extract_text()
+            print(text)
+            table = page.extract_table()
 
-        lines = text.splitlines()
-        for i, line in enumerate(lines):
+            lines = text.splitlines()
+            for i, line in enumerate(lines):
 
-            match = pattern_umsatzsteuer.search(line)
-            if match:
-                value = match.group(1)
-                umsatzsteuer = value
-            if line.strip().startswith("Rechnung Rechnungsadresse:"):
-                if i + 1 < len(lines):
-                    next_line = lines[i + 1].strip()
-                    name = next_line
-            match = pattern_rechnungsdatum.search(line)
-            if match:
-                # nur den Teil vor dem Datum behalten
-                date = match.group(1)  # "Rechn
-    headers = table[0]
-    rows = table[1:]
-    umsatzszeuer_string = str(umsatzsteuer) + "(%)"
-    df = pl.DataFrame(rows, schema=headers)
-    df = df.with_columns([
-        pl.lit(umsatzszeuer_string).alias("Ust(%)"),
-        pl.lit(name).alias("Name"),
-        pl.lit(date).alias("Rechnungsdatum"),
-        (pl.col("Preis").str.replace("€", "")  # € entfernen
-         .str.strip_chars()
-         .str.replace(",", ".")  # führende/abschließende Leerzeichen entfernen
-         .cast(pl.Float64) * float(str(umsatzsteuer).replace(",", ".")) / 100).alias("Ust")
-    ])
-    return df.drop("Po")
-files = ["test.pdf","test2.pdf"]
+                match = pattern_umsatzsteuer.search(line)
+                if match:
+                    value = match.group(1)
+                    umsatzsteuer = value
+                if line.strip().startswith("Rechnung Rechnungsadresse:"):
+                    if i + 1 < len(lines):
+                        next_line = lines[i + 1].strip()
+                        name = next_line
+                match = pattern_rechnungsdatum.search(line)
+                if match:
+                    # nur den Teil vor dem Datum behalten
+                    date = match.group(1)  # "Rechn
+        headers = table[0]
+        rows = table[1:]
+        umsatzszeuer_string = str(umsatzsteuer) + "(%)"
+        df = pl.DataFrame(rows, schema=headers)
+        df = df.with_columns([
+            pl.lit(umsatzszeuer_string).alias("Ust(%)"),
+            pl.lit(name).alias("Name"),
+            pl.lit(date).alias("Rechnungsdatum"),
+            (pl.col("Preis").str.replace("€", "")  # € entfernen
+             .str.strip_chars()
+             .str.replace(",", ".")  # führende/abschließende Leerzeichen entfernen
+             .cast(pl.Float64) * float(str(umsatzsteuer).replace(",", ".")) / 100).alias("Ust")
+        ])
+    except Exception as e:
+        logger.error(f"Fehler beim Verarbeiten der Datei {file}: {e}")
+    return None
+files = list(Path(".").glob("*.pdf"))
 df_all = None
 for file in files:
     df = extract_rechnungen(file)
-    if df_all is None:
-        df_all = df
-    else:
-        df_all = pl.concat([df_all, df], how="vertical")
-print(df_all)
+    if df is not None:
+        if df_all is None:
+            df_all = df
+        else:
+            df_all = pl.concat([df_all, df], how="vertical")
+df.write_excel("rechnungen.xlsx")
